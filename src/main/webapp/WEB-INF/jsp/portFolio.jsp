f713e60643c356af342eff01862c268e46f8f087
computer <computer@DESKTOP-SF4T6MK>
2022-03-24 19:35:37 +0900

    스크롤 이벤트를 통해 동적으로 이미지 보여주기

--- src/main/webapp/WEB-INF/jsp/portFolio.jsp
+++ src/main/webapp/WEB-INF/jsp/portFolio.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <link type="text/css" rel="stylesheet" href="<c:url value='/resources/css/common.css' />" />
@@ -7,16 +7,77 @@
 
 
 <div id="page_container">
-	<div id="main_page" class="c_yelloding">
+	<div id="main_page">
 		<main>
-			<aside>
+			<aside id="left_side">
 				<a id="a_menu" href="#" class="c_blueding">menu</a>
 			</aside>
 			<article>
-				<section>
-					<img alt="image02" src="<c:url value="/resources/images/image02.png"/>">
+				<section id="frst_section" class="c_yelloding"></section>
+				<section class="content_section">
+					<div>
+						<img alt="image02" src="<c:url value="/resources/images/image02.png"/>">
+					</div>
 				</section>
+				<section class="content_section">
+					<div>
+						<img alt="image01" src="<c:url value="/resources/images/image01.png"/>">
+					</div>
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+				<%-- <section id="frst_section" class="c_yelloding"></section>
+				<section class="content_section">
+					<div>
+						<img alt="image02" src="<c:url value="/resources/images/image02.png"/>">
+					</div>
+				</section>
+				<section class="content_section">
+					<div>
+						<img alt="image01" src="<c:url value="/resources/images/image01.png"/>">
+					</div>
+				</section>
+				<div>
+					<span  class="c_blueding">
+						01. &lt;INTRODUCE /&gt;
+					</span>
+				</div>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section>
+				<section class="content_section">
+				</section> --%>
 			</article>
+			<div>
+				<span class="c_blueding">
+					01. &lt;INTRODUCE/&gt;
+				</span>
+			</div>
 		</main>
 	</div>
 </div>
--- src/main/webapp/resources/js/portfolio.js
+++ src/main/webapp/resources/js/portfolio.js
@@ -8,6 +8,12 @@ let portFolioObj = {
 	
 	selector : '#page_wrap',
 	
+	size : {
+		winHeight : null,
+		pageHeight : null
+		
+	},
+	
 	init : function(){
 		let that = this;
 		
@@ -16,23 +22,86 @@ let portFolioObj = {
 	},
 	
 	initUi : function(){
-		//let that = this;
+		let that = this;
 		
-		let contentPage = $('.content_page');
+		that.resizeHandler();
+		that.initClip();
+	},
+	
+	initClip : function(){
+		//let sections = $(".content_section:not(:eq(0))");
+		let sections = $(".content_section");
 		
-		let pageHeight = $(window).height();
-		let pageWidth = $(window).width();
+		for(let i = 1; i < sections.length; i++){
+			let sectionTag = sections[i];
+			let secHeight = $(sectionTag).height();
+			let secWidth = $(sectionTag).width();
+			
+			$(sections).css("clip", "rect(" + secHeight + ", " + secWidth + "," + secHeight + ", 0)");
+		}
+	},
+	
+	resizeHandler : function(){
+		let that = this;
+		
+		that.size.winHeight = $(window).height(); 
+		let contentPage = $('section');
+		
+		$("#page_container").height(that.size.winHeight * contentPage.length);
+		
+		that.size.pageHeight = $("#page_container").height();
 		
 		for(let i = 0; i < contentPage.length; i++){
-			//contentPage[i].style.position = 'absolute';
-			contentPage[i].style.width = pageWidth;
-			contentPage[i].style.height = pageHeight / contentPage.length;
+			contentPage[i].style.height = that.size.pageHeight / contentPage.length;
 		}
 	},
 	
 	bindEvents : function(){
-		//let that = this;
+		let that = this;
 		
+		$(window).resize(function(){
+			that.resizeHandler();
+		});
 		
+		$(window).scroll(function(){
+			that.scrollHandler($(this).scrollTop());
+		});
+	},
+	
+	scrollHandler : function(scrollTop){
+		let that = this;
+		
+		// 섹션들
+		let contentPage = $('.content_section');
+		
+		// 스크롤 위치 / 윈도우 = 몇번째 동작을 취해야 하는지
+		let pageCnt = parseInt(scrollTop / that.size.winHeight);
+		
+		let top = 0;
+		let bottom = 0;
+		
+		// 해당 섹션의 이미지
+		//let selectImg = $(contentPage[sectionCnt]).find("img");
+		if(scrollTop < that.size.winHeight){
+			for(let i = 0; i < contentPage.length; i++){
+				that.imgClip(contentPage[i], that.size.winHeight, that.size.winHeight);
+			}
+		}
+		
+		//if(pageCnt != 0){
+			top = (that.size.winHeight * pageCnt) - scrollTop;
+			bottom = that.size.winHeight;
+			that.imgClip(contentPage[pageCnt], $(contentPage[pageCnt]).height() + top, bottom);
+		//}
+		
+		console.log(pageCnt);
+		//that.imgClip(selectImg, top, bottom);
+	},
+	
+	imgClip : function(selectSection, top, bottom){
+		//let secHeight = $(selectSection).height();
+		let secWidth = $(selectSection).width();
+		
+		$(selectSection).css("clip", "rect(" + top + ", " + secWidth + "," + bottom + ", 0)");
 	}
 };
